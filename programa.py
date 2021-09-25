6667802aef189c324caf78c485505ab8d87d0c63
fenavarrov <90073326+fenavarrov@users.noreply.github.com>
2021-09-24 21:12:38 -0300

    Add files via upload

---
+++ programa.py
@@ -0,0 +1,17 @@
+print("--------------------------------------------")
+print("credito mandell")
+print("--------------------------------------------")
+
+varnombre = input ("ingrese su nombre: ")
+varapellido = input ("ingrese su apellido: ")
+varedad = input ("ingrese su edad: ")
+varcredito = input ("ingrese su monto credto: ")
+varmontosalario = input ("ingrese su sueldo mensual: ")
+
+
+if varcredito > varmontosalario:
+    print("credito supera su sueldo")
+else:
+    print("credito aprobado!!")
+    print("si no paga!!")
+    print("te mato el gato")
