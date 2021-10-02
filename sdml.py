bc37da70b2e30a65f27a798225efec3fef0f637d
fenavarrov <90073326+fenavarrov@users.noreply.github.com>
2021-10-01 21:11:31 -0300

    Add files via upload

---
+++ sdml.py
@@ -0,0 +1,35 @@
+varnumero1 = 0
+varincrementador = 1
+
+varnumero1 = varnumero1 + varincrementador
+
+print(varnumero1)
+
+print("el pipe puro que desordena")
+
+varnumero1 = varnumero1 + varincrementador
+print(varnumero1)
+print("el mendel puro frio")
+print(varnumero1)
+
+
+
+
+
+print("-------------------------------")
+
+varRango = -500
+varRango2 = 0
+
+for varCont in range(varRango, varRango2):
+    print("la secuensia es:"+str(varCont))
+
+
+
+print("______________________")
+
+
+VarArreglo = [a,e,i,o,u]
+
+for varCont in varArreglo:
+    print("la vocales es:"+(varCont))
